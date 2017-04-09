3ca04993f5528a92c22dbc9199235a04d985244e
Johan Andersson <900304@gmail.com>
2017-04-09 13:59:24 +0200

    Further FCM work

--- app/src/main/java/se/rooter/rooterchat/MainActivity.java
+++ app/src/main/java/se/rooter/rooterchat/MainActivity.java
@@ -1,14 +1,21 @@
 package se.rooter.rooterchat;
 
+import android.app.ProgressDialog;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -19,7 +26,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText editTextPassword;
     private TextView textViewSignin;
 
-    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth rooterAuth;
 
     private FirebaseAuth.AuthStateListener mAuthListener;
 
@@ -27,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(this);
 
         buttonRegister = (Button) findViewById(R.id.buttonRegister);
 
@@ -38,7 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonRegister.setOnClickListener(this);
         textViewSignin.setOnClickListener(this);
 
-        mAuth = FirebaseAuth.getInstance();
+        rooterAuth = FirebaseAuth.getInstance();
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -64,5 +75,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (view == textViewSignin) {
             //Login
         }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        // Email empty
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter an email", Toast.LENGTH_SHORT).show();
+            // Stopping further execution
+            return;
+        }
+
+        // Password empty
+        if(TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show();
+            // Stopping further execution
+            return;
+        }
+
+        // Valid input, continue
+
+        progressDialog.setMessage("Registering user..");
+        progressDialog.show();
+
+        rooterAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+            }
+        });
+
     }
 }

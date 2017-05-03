f96a5d691c01e40c25e5c70a619b29b251661977
Johan Andersson <900304@gmail.com>
2017-05-03 18:41:41 +0200

    Doing a proper readme file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ja222um-project
+# Chapp!
+A completely free chat application for Android
+![alt text][channelview]
+
+[channelview]: http://chapp.rooter.se/img/chappchannelview.png "View of a chat channel"
--- app/src/main/java/se/rooter/rooterchat/ConversationsFragment.java
+++ app/src/main/java/se/rooter/rooterchat/ConversationsFragment.java
@@ -6,8 +6,15 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class ConversationsFragment extends Fragment {
+
+    private EditText newConvo;
+    private ImageButton imgBtnStartNew;
+    private ListView conversationsList;
 
     View myView;
 
@@ -15,6 +22,18 @@ public class ConversationsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.conversations_layout, container, false);
+
+        newConvo = (EditText) myView.findViewById(R.id.editTextNewConvo);
+        imgBtnStartNew = (ImageButton) myView.findViewById(R.id.imgBtnStartNew);
+
+        imgBtnStartNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
         return myView;
     }
 }
